feb452a1d0448be1cc3332cf19310b468be26238
= <adrianskevin00@gmail.com>
2022-07-15 20:02:26 +0700

    login

--- src/router/index.js
+++ src/router/index.js
@@ -8,13 +8,14 @@ import {
 import Home from '../Home';
 import Dashboard from '../pages/home/home';
 import Inventory from '../pages/master/inventory/inventory'
+import Login from "../pages/login/login";
 import { connect } from 'react-redux'
 const index = () => {
 
   return (
     <BrowserRouter>
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route path="/" element={<Login />} />
       <Route path="/dashboard" element={<Dashboard />} />
       <Route path="/master/inventory" element={<Inventory target="inventory" />} />
 
